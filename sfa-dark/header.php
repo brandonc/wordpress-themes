055cf413f89f58c06b3de0c4eb6c6e7b846cca65
Brandon Croft <bsc@bscmac.local>
2010-01-12 14:25:08 -0700

    sfa test

---
+++ sfa-dark/header.php
@@ -0,0 +1,22 @@
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Strict//EN"
+	"http://www.w3.org/TR/xhtml1/DTD/xhtml1-strict.dtd">
+
+<html xmlns="http://www.w3.org/1999/xhtml" xml:lang="en" lang="en">
+<head profile="http://gmpg.org/xfn/11">
+	<meta http-equiv="Content-Type" content="<?php bloginfo('html_type'); ?>; charset=<?php bloginfo('charset'); ?>" />
+	<title><?php wp_title('&ndash;', true, 'right'); ?> <?php bloginfo('name'); ?></title>
+	<link rel="stylesheet" href="<?php bloginfo('stylesheet_url'); ?>" type="text/css" media="screen" />
+	<?php wp_head(); ?>
+	<script type="text/javascript" src="<?php bloginfo('stylesheet_directory'); ?>/js/cufon-yui.js"></script>
+	<script type="text/javascript" src="<?php bloginfo('stylesheet_directory'); ?>/js/Rockwell_Std_400.font.js"></script>
+	<script type="text/javascript">
+		Cufon.replace('h1,h2', { fontFamily: 'Rockwell Std' });
+	</script>
+</head>
+<body <?php body_class(); ?>>
+<div id="wrap">
+	<div id="page" class="container_16 clearfix">
+		<div id="header">
+			<div id="logo"><a href="<?php echo get_option('home'); ?>"><img src="<?php bloginfo('stylesheet_directory'); ?>/img/logo.png" alt="Secret Foreign Action"/></a></div>
+		</div>
+		
---
+++ sfa-dark/index.php
@@ -0,0 +1,23 @@
+<?php
+		get_header(); ?>
+		
+		<div class="grid_10">
+		<?php if (have_posts()) : the_post(); ?>
+			<div class="post" id="post-<?php the_ID(); ?>">
+				<?php the_post_thumbnail('single-post-thumbnail'); ?>
+				<h1><?php the_title; ?></h2>
+				<h2><?php the_excerpt; ?></h2>
+			</div>			
+		<?php else: ?>
+			<div class="post">
+				<h1>Either You Shouldn't Be Here,</h1>
+				<h2>Or the blog has been destroyed</h2>
+			</div>
+		<?php endif; ?>
+		</div>
+		<div class="grid_6">
+			<?php bloginfo('description'); ?>
+		</div>
+	</div>
+</div>
+<?php get_footer(); ?>
