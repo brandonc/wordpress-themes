f13a047b95fd0638443cc66fef328b69b5cf0cf1
unknown <brandon@.MondoRobot.hq>
2010-01-15 17:15:47 -0700

    timeline 2.0

--- sfa-dark/footer.php
+++ sfa-dark/footer.php
@@ -51,29 +51,75 @@
 			$('#searchform label').show(0);
 		}
 	});
+
+	function get_president_by_year(year) {
+		year = parseInt(year);
+		if(year >= 1945 && year <= 1953)
+			return 'truman';
+		else if(year > 1953 && year <= 1961)
+			return 'eisenhower';
+		else if(year > 1961 && year <= 1963)
+			return 'kennedy';
+		else if(year > 1963 && year <= 1969)
+			return 'johnson';
+		else if(year > 1969 && year <= 1974)
+			return 'nixon';
+		else if(year > 1974 && year <= 1977)
+			return 'ford';
+		else if(year > 1977 && year <= 1981)
+			return 'carter';
+		else if(year > 1981 && year <= 1989)
+			return 'reagan';
+		else if(year > 1989 && year <= 1993)
+			return 'bush41';
+		else if(year > 1993 && year <= 2001)
+			return 'clinton';
+		else if(year > 2001 && year <= 2009)
+			return 'bush43';
+		else if(year > 2009)
+			return 'obama';
+	}
 	
 	$('.pushpin').hover(function() {
-		var callout = $('<div class="callout"><h4>' +
+		var callout = $('<div class="callout callout-vertical"><h4>' +
 							$(this).find('.title').text() +
 						'</h4><em>' + $(this).find('.info').text() +
 						'</em><p>' + $(this).find('.description').text() +
 						'<a href="' + $(this).find('a').attr('href') + '"> Read More...</a></p></div>');
-						
-		callout.css('left', '40px');
+		
+		callout.css('left', $(this).css('left') - 150);
+		callout.css('top', '-120px');
 		callout.css('display', 'block');
 		callout.css('opacity', '0');
 		$(this).append(callout);
 		Cufon.replace('.callout h4', { fontFamily: 'Rockwell Std', textShadow: '1px 1px #161a1c' });
 		Cufon.now();
 		callout.animate({
-			left: '10px',
+			top: '-90px',
 			opacity: 1.0,
 		}, 200);
+
+		var saturate;
+		var label;
+
+		var president = get_president_by_year(parseInt($(this).find('.year').text()));
+		saturate = $('<div class="president ' + president + ' saturate"></div>');
+		label = $('.' + president + ' label');
+		$('.' + president).append(saturate);
+
+		saturate.animate({
+			opacity: 1.0,
+		}, 500);
 	}, function() {
 		$(this).find('.callout').animate({
-			left: '40px',
+			top: '-120px',
 			opacity: 0.0,
 		}, 200, function() { $(this).remove(); });
+
+		var president = get_president_by_year(parseInt($(this).find('.year').text()));
+		$('.' + president + ' > .saturate').animate({
+			opacity: 0.0,
+		}, 500, function() { $(this).remove(); });
 	});
 </script>
 </body>
--- sfa-dark/page-timeline.php
+++ sfa-dark/page-timeline.php
@@ -3,38 +3,44 @@
 		
 		<div class="pagecontent container_16">
 			<div class="grid_16">
-				<h1>Browse Entries by Executive Timeline</h1>
-			</div>
-			<div class="grid_8 push_1 timeline">
-				<ol class="portraits">
-					<li class="years_8"><label class="lighter">Henry S. Truman</label><img src="<?php bloginfo('template_url') ?>/img/president-truman.jpg" alt="Henry S. Truman (1945-1953)"/></li>
-					<li class="years_8"><label class="lighter">Dwight D. Eisenhower</label><img src="<?php bloginfo('template_url') ?>/img/president-eisenhower.jpg" alt="Dwight D. Eisenhower (1953-1961)"/></li>
-					<li class="years_2"><label class="lighter">John F. Kennedy</label><img src="<?php bloginfo('template_url') ?>/img/president-kennedy.jpg" alt="John F. Kennedy (1961-1963)"/></li>
-					<li class="years_6"><label class="lighter">Lyndon B. Johnson</label><img src="<?php bloginfo('template_url') ?>/img/president-johnson.jpg" alt="Lyndon B. Johnson (1963-1969)"/></li>
-					<li class="years_6"><label class="lighter">Richard M. Nixon</label><img src="<?php bloginfo('template_url') ?>/img/president-nixon.jpg" alt="Richard M. Nixon (1969-1974)"/></li>
-					<li class="years_4"><label class="lighter">Gerald R. Ford</label><img src="<?php bloginfo('template_url') ?>/img/president-ford.jpg" alt="Gerald R. Ford (1974-1977)"/></li>
-				</ol>
+				<h1>Executive Timeline</h1>
+				
 				<div class="timeline-ticks">
 					<?php
 						$startyear = 1945;
-						$lastyear = 1977;
+						$lastyear = 2012;
 						$posts = get_posts(array('numberposts'=>-1));
 						foreach($posts as $post) {
 							$year = get_post_meta($post->ID, 'timeline_year', true);
 							if($year) {
-								$top = (($year-$startyear)*15);
+								$left = (($year-$startyear)*14);
 								echo '<a href="';
 								the_permalink();
-								echo '"><span class="pushpin" style="top: '.$top.'px;"><p class="title">'.$post->post_title.'</p><p class="description">'.$post->post_excerpt.'</p></a>';
+								echo '"><span class="pushpin" style="left: '.$left.'px;"><p class="title">'.$post->post_title.'</p><p class="description">'.$post->post_excerpt.'</p><p class="year">'.$year.'</p></a>';
 							}
 						}
 						
-						for($y = $startyear; $y <= $lastyear; $y += 10) {
-							$top = (($y-$startyear)*15);
-							echo '<span class="year" style="top: '.$top.'px;">'.$y.'</span>';
+						for($y = $startyear; $y <= $lastyear; $y += 5) {
+							$left = (($y-$startyear)*14);
+							echo '<span class="year" style="left: '.$left.'px;">'.$y.'</span>';
 						}
 					?>
 				</div>
+
+				<ol class="portraits">
+					<li class="years_8"><div class="president truman"><label>Henry S. Truman (1945-1953)</label></div></li>
+					<li class="years_8"><div class="president eisenhower"><label>Dwight D. Eisenhower (1953-1961)</label></div></li>
+					<li class="years_2"><div class="president kennedy"><label>John F. Kennedy (1961-1963)</label></div></li>
+					<li class="years_6"><div class="president johnson"><label>Lyndon B. Johnson (1963-1969)</label></div></li>
+					<li class="years_5"><div class="president nixon"><label>Richard M. Nixon (1969-1974)</label></div></li>
+					<li class="years_3"><div class="president ford"><label>Gerald R. Ford (1974-1977)</label></div></li>
+					<li class="years_4"><div class="president carter"><label>James Carter (1977-1981)</label></div></li>
+					<li class="years_8"><div class="president reagan"><label>Ronald Reagan (1981-1989)</label></div></li>
+					<li class="years_4"><div class="president bush41"><label>George H. W. Bush (1989-1993)</label></div></li>
+					<li class="years_8"><div class="president clinton"><label>William J. Clinton (1993-2001)</label></div></li>
+					<li class="years_8"><div class="president bush43"><label>George W. Bush (2001-2009)</label></div></li>
+					<li class="years_3"><div class="president obama"><label>Barack H. Obama (2009)</label></div></li>
+				</ol>
 			</div>
 		</div>
 		<p class="clear">&nbsp;</p>
